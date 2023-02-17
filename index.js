2c237e04122764425d755704c325c6f2f122af40
Dilan Cano <98999407+DilanCano@users.noreply.github.com>
2023-02-17 01:07:06 -0500

    2da libreria

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { random } = require('make-random')
 
-// Return a random integer between 0 and 200
 random(200)
 .then(resp => console.log(resp))
+
+const movieQuote = require("popular-movie-quotes");
+
+console.log(movieQuote.getSomeRandom(10));
